a0018f695c6548803564e788c7625eddc9cfd20d
DomePlaysHD <domeplayshd@outlook.de>
2020-04-19 10:42:41 +0200

    added de.lua

---
+++ locales/de.lua
@@ -0,0 +1,11 @@
+locales['de'] = {
+  ['show_registration'] = 'zeige Registrierungs Menü',
+  ['show_active_character'] = 'zeige aktive Charaktere',
+  ['delete_character'] = 'lösche deinen Charakter und erstelle einen neuen,
+  ['deleted_character'] = 'dein Charakter wurde gelöscht.',
+  ['not_registered'] = 'du hast keinen Charakter registriert.',
+  ['active_character'] = '~b~äktiver Charakter:~s~ %s %s',
+  ['already_registered'] = 'du hast bereits einen Charakter registriert.',
+  ['failed_identity'] = 'Fehler beim Charakter erstellen, Versuchs nochmal oder kontaktiere einen Admin!',
+  ['create_a_character'] = 'du musst einen Charakter erstellen um zu spielen.'
+}
